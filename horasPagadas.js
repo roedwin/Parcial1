9908286f503edab60f0ac954d8d5365ad882c6ce
roedwin <roedwin92@gmail.com>
2020-09-08 22:28:56 -0600

    Errores y Retornos incluidos

--- horasPagadas.js
+++ horasPagadas.js
@@ -1,3 +1,20 @@
-    exports.pagoMes =  (precioHora,horasTrabajas) => ((precioHora*horasTrabajas)*20);
-    exports.pagos =  (precioHora, personas,horasTrabajas) => ((precioHora*horasTrabajas)*personas);
-    exports.holgura =  (precioHora, horasTrabajas) => ((precioHora*horasTrabajas)/0.08);
+/*exports.pagoMes =  (precioHora,horasTrabajas) => ((precioHora*horasTrabajas)*20);
+exports.pagos =  (precioHora, personas,horasTrabajas) => ((precioHora*horasTrabajas)*personas);
+exports.holgura =  (precioHora, horasTrabajas) => ((precioHora*horasTrabajas)/0.08);*/
+
+module.exports = (precioHora,personas,horasTrabajas, materiales,callback) => {
+    if (precioHora<=0||personas<=0||horasTrabajas<=0||materiales<=0)
+        setTimeout(() => 
+            callback(
+                new Error("No hay presupuesto"), 
+                null),
+            2000);
+    else
+        setTimeout(() => 
+            callback(null, {
+                pagoMes:  (precioHora,horasTrabajas) => ((precioHora*horasTrabajas)*20),
+                pagos: (precioHora, personas,horasTrabajas) => ((precioHora*horasTrabajas)*personas),
+                holgura: (precioHora, horasTrabajas) => ((precioHora*horasTrabajas)/0.08)
+            }), 
+            2000);
+}
