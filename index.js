200a7ab315026d386e27f3b6b3a778a79e81e26e
roedwin <roedwin92@gmail.com>
2020-09-08 21:32:33 -0600

    Parcial Terminado en ultimo momento :)

---
+++ index.js
@@ -0,0 +1,44 @@
+var horasPagadas = {
+    pagoMes: (precioHora,horasTrabajas) => ((precioHora*horasTrabajas)*20),
+    pagos: (precioHora, personas,horasTrabajas) => ((precioHora*horasTrabajas)*personas),
+    holgura: (precioHora, horasTrabajas) => ((precioHora*horasTrabajas)/0.08)
+};
+function costoTotal(precioHora,personas,horasTrabajas, materiales) {
+    console.log("Costo total de gastos en el proyecto");
+    if (precioHora <= 0 || personas <= 0) {
+        console.log("No hay presupuesto");
+    }else{
+        var pagoBruto = (horasPagadas.pagoMes(precioHora,horasTrabajas))*4;
+        var hol = horasPagadas.holgura(precioHora,horasTrabajas);
+        var cobro = horasPagadas.pagos(precioHora,personas,horasTrabajas);
+        var totalPago = (pagoBruto-hol);
+        var totalPagos = (cobro*20)*4;
+        var costoT = totalPagos+materiales;
+        console.log("Una Persona cobra por dia tomando en cuenta que trabajo 8 horas diarias por 4 meses: $" + pagoBruto);
+        console.log("La holgura retenida en caso de accidentes por persona es: $" + hol);
+        console.log("Total pagado por persona: $" + totalPago);
+        console.log("Total pagado a 15 personas en el proyecto: $" + totalPagos);
+        console.log("Costos total incluyendo los materiales: $" + costoT);
+    }
+}
+costoTotal(8,15,8,50000);
+
+/*var rectangulo = {
+	perimetro: (x, y) => (2*(x+y)),
+	area: (x, y) => (x*y)
+};
+function resolverRectangulo(l,b) {
+    console.log("Resolver el rectángulo con = l " + l + " y b = " + b);
+    if (l <= 0 || b <= 0) {
+        console.log("Las dimensiones del rectángulo deben ser mayores a cero:  l = "
+               + l + ",  and b = " + b);
+    }
+    else {
+	    console.log("El area del rectangulo es: " + rectangulo.area(l,b));
+	    console.log("El perimetro del rectángulo es: " + rectangulo.perimetro(l,b));
+    }
+}
+resolverRectangulo(2,4);
+resolverRectangulo(3,5);
+resolverRectangulo(0,5);
+resolverRectangulo(-3,5);*/
